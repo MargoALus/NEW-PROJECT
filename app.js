64c6f44daf2f3e0fa5b8037c71e150ff614f6905
Margo Lusinchi <margolusinchi@Margos-MBP-2.lan>
2019-08-08 12:37:35 +0100

    test commit

---
+++ app.js
@@ -0,0 +1,3 @@
+var msg = 'Hello World'; 
+console.log(msg);
+//so is msg just describing what the string is? seems almost like a shortcut because the computer already knows (basically) the purpose of this variable. 
